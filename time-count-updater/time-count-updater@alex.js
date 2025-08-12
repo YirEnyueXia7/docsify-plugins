dabc90832590e996a6cf5f1efd0ead8a0465715e
YirEnyueXia7 <43158124+YirEnyueXia7@users.noreply.github.com>
2025-08-12 16:35:33 +0800

    Update time-count-updater@alex.js

--- time-count-updater/time-count-updater@alex.js
+++ time-count-updater/time-count-updater@alex.js
@@ -7,19 +7,13 @@ let defaultDocsifyUpdatedOptions = {
 
 // Docsify plugin functions
 function plugin(hook, vm) {
-  // 嫁接计数代码
   let wordsCount
-  // 原time-updater代码
   let text = vm.config.timeUpdater.text
   let whereToPlace = String(vm.config.timeUpdater.whereToPlace).toLowerCase()
   hook.beforeEach(function (content) {
-    // 嫁接计数代码
-    // Match regex every time you start parsing .md
     wordsCount = content.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length
-    // 替换文本中的占位符
     let finalText = text
       .replace('{words-count}', wordsCount)
-    // 原time-updater代码
     switch (whereToPlace) {
       case "top":
         return finalText + "\n\n" + content
@@ -30,7 +24,6 @@ function plugin(hook, vm) {
     }
   })
 }
-
 // Docsify plugin options
 window.$docsify = (window.$docsify || {})
 // https://docsify.js.org/#/configuration?id=formatupdated
